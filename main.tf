203833f4fd0d9ac29a94d681c7677f32b2971fcb
sfc-gh-nrajaofetra <94199571+sfc-gh-nrajaofetra@users.noreply.github.com>
2022-06-07 19:51:31 +0200

    Add service user, Schema, grants v4

--- main.tf
+++ main.tf
@@ -80,16 +80,6 @@ resource "snowflake_warehouse" "warehouse" {
     }
 
 
-    resource "snowflake_user" "user" {
-        provider          = snowflake.security_admin
-        name              = "NRA_TF_SNOW"
-        default_warehouse = snowflake_warehouse.warehouse.name
-        default_role      = snowflake_role.role.name
-        default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
-        rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
-    }
-
-
     resource "snowflake_role_grants" "grants" {
         provider  = snowflake.security_admin
         role_name = snowflake_role.role.name
